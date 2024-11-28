a5e0d43ac2f9fe53b7b40b4ca9c4265b437ef612
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2024-11-28 20:31:24 +0700

    My First config for openbox

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# openbox-settings
-My Config for Openbox
+# My peak Openbox dotfiles
+This is my personal Openbox configuration. It is a work in progress and will be updated as I continue to tweak it.  
+Before you use this configuration, please make sure you have the following packages installed (for example in Arch-based distributions):
+```bash
+sudo pacman -S openbox betterlockscreen btop dunst fastfetch kvantum-qt5 mpd mpv ncmpcpp obmenu-generator qt5ct rofi thunar tint2 viewnior xfce4-power-manager spectacle nitrogen rxvt-unicode-truecolor-wide-glyphs nm-applet picom perl-gtk3 alsa-utils brightnessctl imagemagick scrot w3m wireless_tools xclip xsettingsd xss-lock thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler gsimplecal pavucontrol polkit-gnome nvim neovide
+```
+You should use my neovim configuration for the best experience.  
+You can find it [here](https://github.com/nhktmdzhg/nvim).  
+## Screenshots
+![Screenshot 1](meo/screenshot.png)
---
+++ etc/skel/.config/tint2/executor/battery.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+# Desc:   Get status of battery and print icon that is charged or not
+
+export LANG='POSIX'
+exec 2>/dev/null
+. "${HOME}/.joyfuld"
+
+[ -x "$(command -v upower)" ] || exit 1
+
+BATTERRY_STATUS="$(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $2}')"
+BATTERRY_PERCENTAGE="$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}' | tr -d '%')"
+BATTERRY_ICON=''
+ICON_BATTERY_EMPTY=''
+ICON_BATTERY_QUARTER=''
+ICON_BATTERY_HALF=''
+ICON_BATTERY_THREE_QUARTERS=''
+ICON_BATTERY_FULL=''
+ICON_BATTERY_CHARGING=''
+if [ "${BATTERRY_STATUS}" = 'discharging' ]; then
+    if [ "${BATTERRY_PERCENTAGE}" -le 10 ]; then
+        BATTERRY_ICON="${ICON_BATTERY_EMPTY}"
+    elif [ "${BATTERRY_PERCENTAGE}" -le 30 ]; then
+        BATTERRY_ICON="${ICON_BATTERY_QUARTER}"
+    elif [ "${BATTERRY_PERCENTAGE}" -le 50 ]; then
+        BATTERRY_ICON="${ICON_BATTERY_HALF}"
+    elif [ "${BATTERRY_PERCENTAGE}" -le 80 ]; then
+        BATTERRY_ICON="${ICON_BATTERY_THREE_QUARTERS}"
+    else
+        BATTERRY_ICON="${ICON_BATTERY_FULL}"
+    fi
+else
+    BATTERRY_ICON="${ICON_BATTERY_CHARGING}"
+fi
+
+# print icon only
+echo "${BATTERRY_ICON}"
+
+exit ${?}
