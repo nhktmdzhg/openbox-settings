c062b5317f48ae3e37c95be716bd3ac2819456e5
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2025-04-25 17:27:23 +0700

    use playerctl

--- home/username/.scripts/music-controller.sh
+++ home/username/.scripts/music-controller.sh
@@ -3,25 +3,22 @@
 export LANG='POSIX'
 exec >/dev/null 2>&1
 
-# Check if the music controller is already running
-IS_RUNNING=$(rhythmbox-client --check-running)
-if [ "${IS_RUNNING}" = "false" ]; then
-    exit 1
-fi
-
 action="$1"
 case "${action}" in
     prev)
-        rhythmbox-client --previous
+        playerctl previous
         ;;
     next)
-        rhythmbox-client --next
-        ;;
-    toggle)
-        rhythmbox-client --play-pause
+        playerctl next
         ;;
     stop)
-        rhythmbox-client --stop
+        playerctl stop
+        ;;
+    play)
+        playerctl play-pause
+        ;;
+    pause)
+        playerctl play-pause
         ;;
     *)
         ;;
